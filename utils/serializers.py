485ce674864250f3ef6e4d686933d2a5cdb5d9e4
huangheshuai <1692717842@qq.com>
2024-07-29 10:34:58 +0800

    添加自定义CustomModelSerializer

--- utils/serializers.py
+++ utils/serializers.py
@@ -6,73 +6,125 @@ from rest_framework import serializers
 from rest_framework.fields import empty
 from rest_framework.request import Request
 from django.contrib.auth import get_user_model
-from apps.users.signals import generate_user_id
 
 Users = get_user_model()
 
 
 class CustomModelSerializer(serializers.ModelSerializer):
     """
-    增强DRF的ModelSerializer, 可自动更新模型的审计字段记录
-        - self.request 能获取到 rest_framework.request.Request 对象
+    增强DRF的ModelSerializer,自动更新模型的审计字段记录
+    1. self.request 能获取到 rest_framework.request.Request 对象
     """
-
-    def __init__(self, instance=None, data=serializers.empty, request=None, **kwargs):
-        super().__init__(instance, data, **kwargs)
-        self.request: Request = request or self.context.get('request')
-
     # 修改人的审计字段名称, 默认 modifier, 继承使用时可自定义覆盖
-    modifier_field_name = 'modifier'
+    modifier_field_id = 'modifier'
+    # 修改人姓名的序列化字段, 只读
     modifier_name = serializers.SerializerMethodField(read_only=True)
 
-    def get_modifier_name(self, instance):
-        if not hasattr(instance, self.modifier_field_name):
+    @staticmethod
+    def get_modifier_name(instance):
+        """
+        获取修改人的姓名
+        :param instance: 模型实例
+        :return: 修改人的姓名
+        """
+        if not hasattr(instance, 'modifier'):
             return None
-        user_name = Users.objects.filter(uuid=getattr(instance, self.modifier_field_name)).values_list('name',
-                                                                                                       flat=True).first()
-        return user_name
+        queryset = Users.objects.filter(id=instance.modifier).values_list('name', flat=True).first()
+        if queryset:
+            return queryset
+        return None
 
     # 创建人的审计字段名称, 默认 creator, 继承使用时可自定义覆盖
-    creator_field_name = 'creator'
+    creator_field_id = 'creator'
+    # 创建人姓名的序列化字段, 只读
     creator_name = serializers.SlugRelatedField(slug_field="name", source="creator", read_only=True)
+
     # 数据所属部门字段
+    dept_belong_id_field_name = 'dept_belong_id'
+
     # 添加默认时间返回格式
     create_datetime = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", required=False, read_only=True)
     update_datetime = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", required=False)
 
+    def __init__(self, instance=None, data=empty, request=None, **kwargs):
+        """
+        初始化方法
+        :param instance: 模型实例
+        :param data: 序列化数据
+        :param request: 请求对象
+        :param kwargs: 其他参数
+        """
+        super().__init__(instance, data, **kwargs)
+        # 将 self.request 设置为传入的 request 参数，如果没有传入 request 参数，则从 self.context 中获取 request 对象。
+        self.request: Request = request or self.context.get('request', None)
+
+    def save(self, **kwargs):
+        """
+        保存方法
+        :param kwargs: 其他参数
+        :return: 保存的实例
+        """
+        return super().save(**kwargs)
+
     def create(self, validated_data):
-        if self.request and self.request.user.is_authenticated:
-            if self.modifier_field_name in self.fields.fields:
-                validated_data[self.modifier_field_name] = self.get_request_user_id()
-            if self.creator_field_name in self.fields.fields:
-                validated_data[self.creator_field_name] = self.request.user
-
-        instance = super().create(validated_data)
-
-        # 调用 generate_user_id 处理 user_id 生成逻辑，传递实例而不是 validated_data
-        generate_user_id(sender=self.__class__, instance=instance)
-
-        return instance
+        """
+        创建方法
+        :param validated_data: 验证后的数据
+        :return: 创建的实例
+        """
+        if self.request:
+            if str(self.request.user) != "AnonymousUser":
+                # 设置修改人
+                if self.modifier_field_id in self.fields.fields:
+                    validated_data[self.modifier_field_id] = self.get_request_user_id()
+                # 设置创建人
+                if self.creator_field_id in self.fields.fields:
+                    validated_data[self.creator_field_id] = self.request.user
+                # 设置所属部门
+                if self.dept_belong_id_field_name in self.fields.fields and not validated_data.get(self.dept_belong_id_field_name, None):
+                    validated_data[self.dept_belong_id_field_name] = getattr(self.request.user, 'dept_id', None)
+        return super().create(validated_data)
 
     def update(self, instance, validated_data):
-        if self.request and self.request.user.is_authenticated:
-            if self.modifier_field_name in self.fields.fields:
-                validated_data[self.modifier_field_name] = self.get_request_user_id()
-            if hasattr(instance, self.modifier_field_name):
-                setattr(instance, self.modifier_field_name, self.get_request_user_id())
-
-        instance = super().update(instance, validated_data)
-
-        # Call generate_user_id here if needed
-        generate_user_id(sender=self.__class__, instance=instance)
-
-        return instance
+        """
+        更新方法
+        :param instance: 要更新的实例
+        :param validated_data: 验证后的数据
+        :return: 更新的实例
+        """
+        if self.request:
+            if str(self.request.user) != "AnonymousUser":
+                # 设置修改人
+                if self.modifier_field_id in self.fields.fields:
+                    validated_data[self.modifier_field_id] = self.get_request_user_id()
+            # 更新实例的修改人字段
+            if hasattr(self.instance, self.modifier_field_id):
+                setattr(self.instance, self.modifier_field_id, self.get_request_user_id())
+        return super().update(instance, validated_data)
 
     def get_request_username(self):
-        return getattr(self.request.user, 'username', None) if self.request and self.request.user else None
+        """
+        获取当前请求用户的用户名
+        :return: 当前请求用户的用户名
+        """
+        if getattr(self.request, 'user', None):
+            return getattr(self.request.user, 'username', None)
+        return None
 
     def get_request_name(self):
-        return getattr(self.request.user, 'name', None) if self.request and self.request.user else None
+        """
+        获取当前请求用户的姓名
+        :return: 当前请求用户的姓名
+        """
+        if getattr(self.request, 'user', None):
+            return getattr(self.request.user, 'name', None)
+        return None
 
     def get_request_user_id(self):
-        return getattr(self.request.user, 'user_id', None) if self.request and self.request.user else None
+        """
+        获取当前请求用户的ID
+        :return: 当前请求用户的ID
+        """
+        if getattr(self.request, 'user', None):
+            return getattr(self.request.user, 'id', None)
+        return None
