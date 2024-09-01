ece047ac7304afe4566464d7905d31bf4d37bed7
oscar <2251018029@qq.com>
2024-09-01 11:44:53 +0800

    集合竞价涨停显示优化

--- apps/stock_picker/adminx.py
+++ apps/stock_picker/adminx.py
@@ -11,6 +11,7 @@ from django.core.cache import cache
 from apps.stock_picker.models import (
     StockAuction,
     StockLimitUpAuction,
+    StockLimitUpAuctionReal,
     StockAuctionConditions,
 )
 from utils.common import format_color
@@ -105,8 +106,84 @@ class StockLimitUpAuctionAdmin(object):
     trade_date_color.short_description = '交易日'
 
 
+class StockLimitUpAuctionRealAdmin(object):
+    list_display = ['trade_date_color', 'stock_code', 'stock_name', 'limit_up_days', 'vol_ratio_color',
+                    'vol_ratio_oa_color', 'cap_color', 'open_price', 'pre_close', 'model_name', 'last_limit_up_time',
+                    'limit_up_reasons', 'is_ops', 'profit_chg', 'profit_chg_close', 'cb', 'update_datetime']
+    list_filter = ['trade_date', 'stock_code', 'stock_name', 'is_ops', 'limit_up_days']
+    search_fields = ['trade_date', 'stock_code', 'stock_name', 'is_ops', 'limit_up_days']
+    # 排序字段
+    # ordering = ['-trade_date', '-limit_up_days', '-pre_open_vol_ratio']
+    list_per_page = 15
+    list_display_links = ['trade_date']
+    list_editable = []
+    model_icon = 'fa fa-thumbs-o-up'
+
+    def queryset(self):
+        from django.db.models import F, Window
+        from django.db.models.functions import RowNumber
+        qs = super().queryset()
+        latest_date = qs.latest('trade_date').trade_date
+
+        # 使用 Window 函数为每个 limit_up_reasons 分组添加 row_number，并按 cap 从小到大排序
+        annotated_qs = qs.filter(
+            trade_date=latest_date
+        ).annotate(
+            row_number=Window(
+                expression=RowNumber(),
+                partition_by=[F('limit_up_reasons')],
+                order_by=F('cap').asc()  # 确保这里是升序排序
+            )
+        )
+
+        # 按 row_number 排序
+        return annotated_qs.order_by('row_number')
+
+    # 天量比
+    def vol_ratio_color(self, obj):
+        thresholds = [
+            lambda x: 0.09 < x <= 0.20,
+            lambda x: x > 0.20
+        ]
+        colors = ['red', 'blue']
+        return format_color(obj.pre_open_vol_ratio, thresholds, colors)
+
+    vol_ratio_color.short_description = '天量比'
+
+    # 分量比
+    def vol_ratio_oa_color(self, obj):
+        thresholds = [
+            lambda x: 0.70 <= x <= 2.0,
+            lambda x: x > 2.0
+        ]
+        colors = ['red', 'blue']
+        return format_color(obj.pre_open_max_vol_ratio, thresholds, colors)
+
+    vol_ratio_oa_color.short_description = '分量比'
+
+    # 市值(亿)
+    def cap_color(self, obj):
+        thresholds = [
+            lambda x: x < 30
+        ]
+        colors = ['red']
+        return format_color(obj.cap, thresholds, colors)
+
+    cap_color.short_description = '市值(亿)'
+
+    def trade_date_color(self, obj):
+        current_date = datetime.now().date()
+        thresholds = [
+            lambda x: x == current_date
+        ]
+        colors = ['red']
+        return format_color(obj.trade_date, thresholds, colors)
+
+    trade_date_color.short_description = '交易日'
+
+
 class StockAuctionConditionsAdmin(BaseColorAdmin):
-    list_display = ['trade_date_color', 'stock_code', 'stock_name', 'vol_ratio_color', 'vol_ratio_oa_color', 'cap', 'gap_type_color', 'cond_name', 'open_price', 'pre_close', 'high_price', 'profit_chg_color', 'profit_chg_close', 'is_ops', 'update_datetime']
+    list_display = ['trade_date_color', 'stock_code', 'stock_name', 'vol_ratio_color', 'vol_ratio_oa_color', 'open_price', 'profit_chg_color', 'cap', 'gap_type_color', 'cond_name', 'concept', 'is_ops', 'profit_chg_close', 'update_datetime']
     list_filter = ['trade_date', 'stock_code', 'stock_name', 'is_ops', 'gap_type', 'is_ops']
     search_fields = ['trade_date', 'stock_code', 'stock_name', 'is_ops', 'gap_type', 'is_ops']
     ordering = ['-trade_date',  '-profit_chg']
@@ -143,7 +220,7 @@ class StockAuctionConditionsAdmin(BaseColorAdmin):
         colors = ['red']
         return format_color(obj.profit_chg, thresholds, colors)
 
-    profit_chg_color.short_description = '竞价盈亏'
+    profit_chg_color.short_description = '竞价涨幅'
 
     def trade_date_color(self, obj):
         current_date = datetime.now().date()
@@ -166,6 +243,7 @@ class StockAuctionConditionsAdmin(BaseColorAdmin):
 
 xadmin.site.register(StockAuction, StockAuctionAdmin)
 xadmin.site.register(StockLimitUpAuction, StockLimitUpAuctionAdmin)
+xadmin.site.register(StockLimitUpAuctionReal, StockLimitUpAuctionRealAdmin)
 xadmin.site.register(StockAuctionConditions, StockAuctionConditionsAdmin)
 
 
