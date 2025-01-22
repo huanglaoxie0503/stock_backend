68dbd9e23898896e0342ba60538aa7235d29b746
huangheshuai <2251018029@qq.com>
2025-01-22 21:32:00 +0800

    增加破局数据

--- apps/base_data/models.py
+++ apps/base_data/models.py
@@ -231,6 +231,6 @@ class DailyConceptLeaders(BaseModel):
     class Meta:
         db_table = table_prefix + 'daily_concept_leaders'
         verbose_name = '概念龙头'
-        verbose_name_plural = '每日概念龙头'
+        verbose_name_plural = '概念龙头'
         unique_together = ('trade_date', 'concept')  # 添加唯一联合索引
 
